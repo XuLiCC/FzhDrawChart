1e59371a85e7f67654af2153e7d31aecca083821
Chrios <lianjin@lianjindeMacBook-Pro.local>
2018-01-16 15:34:14 +0800

    Initial commit
    0

---
+++ Demo/DrawPicture/AppDelegate.h
@@ -0,0 +1,16 @@
+
+//  DrawPicture
+//
+//  Created by 炼金 on 2017/11/1.
+//  Copyright © 2017年 炼金. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/DrawPicture/ViewController.h
@@ -0,0 +1,14 @@
+
+//  DrawPicture
+//
+//  Created by 炼金 on 2017/11/1.
+//  Copyright © 2017年 炼金. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
